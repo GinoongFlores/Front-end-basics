f67507bcebd3d84eea9d78ba815dedbf1886d886
Christian Paul Flores <f.christianpaul@outlook.com>
2022-05-16 12:52:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let Name = "Juan";
+// let lastName = " Del Cruz";
+
+// console.log("May name is " + Name + lastName);
+
+let message = "Hello Nocturnals!";
+let whereami = "I'm here at script.js";
+console.log(message + "\n" + whereami);
